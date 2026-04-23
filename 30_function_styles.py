783bb345a2840292d00a3f65b084ab64545d9a8f
Rojina <rozhina.agh@gmail.com>
2026-04-23 18:37:30 +0330

    Create 30_function_styles.py

---
+++ 30_function_styles.py
@@ -0,0 +1,7 @@
+# For example, we have a bird called henry.
+
+describe_pet('henry')
+describe_pet(pet_name='henry')
+describe_pet('henry','bird')
+describe_pet(pet_name='henry',animal_type='bird')
+describe_pet(animal_type='bird',pet_name='henry')
