72bd2ca617fdcf4bb7aff5c182e4565af734c8a2
Rojina <rozhina.agh@gmail.com>
2026-04-23 18:40:22 +0330

    Create 40_class_example_pt2.py

---
+++ 40_class_example_pt2.py
@@ -0,0 +1,62 @@
+# We want to make a class for cars.
+
+class Car():
+	"""A simple attempt to make a car."""
+
+	def __init__(self,make,model,year):
+		"""Initialize attributes to describe a car. """
+		self.make = make
+		self.model = model
+		self.year = year
+		self.odometer_reading = 0
+
+	def get_name(self):
+		"""Return a neatly formatted descriptive name."""
+		long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+		return long_name.title()
+
+	def read_odometer(self):
+		"""Print a statement showing the car's mileage."""
+		print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+	def update_odometer(self, mileage):
+		"""Set the odometer reading to the given value, reject the change if it attempts to roll the odometer back."""
+		if mileage >= self.odometer_reading:
+			self.odometer_reading = mileage
+		else:
+			print("You can't roll back an odometer!")
+
+	def incerement(self,miles):
+		"""Add the given amount to odometer reading."""
+		self.odometer_reading += miles
+
+class Battery():
+	"""A simple attempt to model a battery for an electric car."""
+	
+	def __init__(self, battery_size = 70):
+		"""initialize the battery"s attributes."""
+		self.battery_size = battery_size
+
+	def describe_battery(self):
+		"""Print a statement describing the battery size. """
+		print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+class ElectricCar(Car):
+	"""A simple attempt to module an electric car."""
+	def __init__(self,make,model,year):
+		"""Initialize attributes of the parent class."""
+		super().__init__(make,model,year)
+		self.battery = Battery()
+
+	def describe_battery(self):
+		"""Print a statement describing the battery size."""
+		print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+
+
+
+my_new_car = Car('BMW' , 'm5' , 2016)
+print(my_new_car.get_name())
+my_tesla = ElectricCar('tesla','model s', 2016)
+print(my_tesla.get_name())
+my_tesla.battery.describe_battery()
