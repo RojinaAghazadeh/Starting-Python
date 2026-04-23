4c35ac16c1927887cb6fb31e899d8266934c3db2
Rojina <rozhina.agh@gmail.com>
2026-04-23 18:31:18 +0330

    Create 8_question_using_input.py

---
+++ 8_question_using_input.py
@@ -0,0 +1,7 @@
+# This program says hello and asks for your name and your age.
+print('What is your name?')
+myName = input()
+print('HI, ' + myName.title())
+print('How old are you ' + myName.title() + "?")
+myAge = input()
+print('Oh, I am not ' + str(int(myAge)) + ' years old.')
