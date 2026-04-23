65b4c72dd5a0883f7754088b7d65b6e16a734552
Rojina <rozhina.agh@gmail.com>
2026-04-23 18:43:40 +0330

    Create names.py

---
+++ names.py
@@ -0,0 +1,13 @@
+from name_function import get_formatted_name
+
+print("Enter 'q' to quit.")
+while True:
+	first = input("\nGive me a first name: ")
+	if first == 'q':
+		break
+	last = input("Give me a last name: ")
+	if last == 'q':
+		break
+
+	formatted_name = get_formatted_name(first, last)
+	print("\n\tNeatly formatted name: " + formatted_name + ".")
