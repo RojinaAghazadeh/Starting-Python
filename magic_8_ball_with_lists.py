c13943cf68d28625267e2835ef5f2968d90f1a82
Rojina <rozhina.agh@gmail.com>
2026-04-23 18:42:49 +0330

    Create magic_8_ball_with_lists.py

---
+++ magic_8_ball_with_lists.py
@@ -0,0 +1,5 @@
+import random
+
+messages = ["It is certain" , "Yes" , "No" , "Outlook not so good" , "very doubtful", "Ask again later" , "Try again" , "Ask again"]
+
+print(messages[random.randit(0, len(messages) - 1)])
