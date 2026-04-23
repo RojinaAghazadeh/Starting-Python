619c831e1b60c933705c3cebcbebc87a63acf016
Rojina <rozhina.agh@gmail.com>
2026-04-23 18:31:33 +0330

    Create 9_loop_in_lists.py

---
+++ 9_loop_in_lists.py
@@ -0,0 +1,6 @@
+students = ['Sara','ava','Emma','may']
+for student in students:
+	print(student.title() + ", You are a nice student.")
+	print("Can't wait to see you again!" + "\n")
+print("Bye everyone!" + "\n")
+
