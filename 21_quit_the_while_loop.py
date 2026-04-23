661d093442b4a80223f9ee2efd61d2583db12e40
Rojina <rozhina.agh@gmail.com>
2026-04-23 18:35:18 +0330

    Create 21_quit_the_while_loop.py

---
+++ 21_quit_the_while_loop.py
@@ -0,0 +1,8 @@
+message = "\nTell me what you want to hear: "
+message += "\nEnter 'quit' to end the program. "
+
+message_0 = " "
+while message_0 != 'quit':
+	message_0 = input(message)
+	print(message_0)
+
