a62ebd0c2a8cc63214bf1738aa73748a0d1e725e
Rojina <rozhina.agh@gmail.com>
2026-04-23 18:39:39 +0330

    Create 37_arbitrary_keyword.py

---
+++ 37_arbitrary_keyword.py
@@ -0,0 +1,11 @@
+def build_profile(first,last,**user_info):
+	"""Build a dictionary containing all we know about a user."""
+	profile = {}
+	profile['first_name'] = first
+	profile['last_name'] = last
+	for key, value in user_info.items():
+		profile[key] = value
+	return profile
+
+user_profile = build_profile('rojina','aghazadeh', location='tehran',field='mathematics')
+print(user_profile)
