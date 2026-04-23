63363c60b077450e56dc2e3a72a151a810184883
Rojina <rozhina.agh@gmail.com>
2026-04-23 18:32:08 +0330

    Create 11_slicing_lists.py

---
+++ 11_slicing_lists.py
@@ -0,0 +1,9 @@
+# Here is how to cut a list
+numbers = ['one','two','three','four','five','six']
+print(numbers[0:3])
+
+# and without starting index, python starts at the beginning.
+print(numbers[:4])
+
+# the reverse form, starts from the end of the list.
+print(numbers[1:])
